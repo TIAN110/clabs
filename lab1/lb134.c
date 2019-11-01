b7b301327d2724d3a9b826aadda47ca1a091c7b7
TIAN110 <1530611124@qq.com>
2019-10-31 21:12:07 -0700

    lab1 homework

---
+++ lab1/lb134.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+main()
+{
+    float celsius, fahr;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 100;
+    step = 5;
+
+    celsius = lower;
+    printf("celsius\tfahr\n");
+    while (celsius <= upper){
+        fahr = (9.0 * celsius) / 5.0 + 32.0;
+        printf("%3.0f %6.1f\n", celsius, fahr);
+        celsius = celsius +step;
+    }
+}
