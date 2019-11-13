2f49ab68de0d7cc4f3b402e7460760c232276cf7
TIAN110 <1530611124@qq.com>
2019-11-13 04:21:35 -0800

    Lab2 homework

--- Lab2/ex3.c
+++ Lab2/ex3.c
@@ -1,13 +1,34 @@
 #include<stdio.h>
-void xtod(int x);
+#define yes 1
+#define no 0
 int main()
 {
-    int i;
-    scanf("%x", &i);
-    xtod(i);
-    return 0;
+
+return 0;
 }
-void xtod (int x)
+int htoi(char s[])
 {
-    printf("%d\n", x);
+int hexdigit ,n, inhex,i;
+i=0;
+if(s[i]=='0')
+{
+  i++;
+  if(s[i]=='x'||s[i]=='X')
+  {
+   i++;
+ }
 }
+n=0;
+inhex=yes;
+for(;inhex==yes;i++)
+{
+  if(s[i]>='0'&&s[i]<='9') hexdigit=s[i]-'0';
+  else if(s[i]>='a'&&s[i]<='f')hexdigit=s[i]-'a'+10;
+  else if(s[i]>='A'&&s[i]<='F')hexdigit=s[i]-'A'+10;
+  else inhex=no;
+  if(inhex==yes)
+    n=16*n+hexdigit;
+}
+return n;
+}
+
