fac1bae3236920ae4003f698e4b65f7daed48368
TIAN110 <1530611124@qq.com>
2019-11-13 05:44:54 -0800

    Lab2 homework

--- Lab2/ex3.c
+++ Lab2/ex3.c
@@ -8,7 +8,7 @@ return 0;
 }
 int htoi(char s[])
 {
-int hexdigit ,n, inhex,i;
+int hexdigit ,i, inhex,n;
 i=0;
 if(s[i]=='0')
 {
@@ -22,13 +22,18 @@ n=0;
 inhex=yes;
 for(;inhex==yes;i++)
 {
-  if(s[i]>='0'&&s[i]<='9') hexdigit=s[i]-'0';
-  else if(s[i]>='a'&&s[i]<='f')hexdigit=s[i]-'a'+10;
-  else if(s[i]>='A'&&s[i]<='F')hexdigit=s[i]-'A'+10;
-  else inhex=no;
+  if(s[i]>='0'&&s[i]<='9') 
+      hexdigit=s[i]-'0';
+  else if(s[i]>='a'&&s[i]<='f')
+      hexdigit=s[i]-'a'+10;
+  else if(s[i]>='A'&&s[i]<='F')
+      hexdigit=s[i]-'A'+10;
+  else 
+      inhex=no;
   if(inhex==yes)
     n=16*n+hexdigit;
 }
 return n;
 }
 
+ 
